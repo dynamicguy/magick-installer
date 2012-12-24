163c579bbf556c7200a0a3d180c9c0f75e9184d6
Nurul Ferdous <hello@dynamicguy.com>
2012-12-24 12:49:13 +0600

    bumped imagemagick version to 6.8

--- magick-installer.sh
+++ magick-installer.sh
@@ -23,7 +23,7 @@ download http://voxel.dl.sourceforge.net/project/wvware/libwmf/0.2.8.4/libwmf-0.
 download http://downloads.sourceforge.net/project/lcms/lcms/1.19/lcms-1.19.tar.gz
 download http://sourceforge.net/projects/ghostscript/files/GPL%20Ghostscript/9.04/ghostscript-9.04.tar.gz
 download http://voxel.dl.sourceforge.net/project/gs-fonts/gs-fonts/8.11%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-8.11.tar.gz
-download ftp://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/ImageMagick-6.6.7-0.tar.gz
+download ftp://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/ImageMagick-6.8.1-2.tar.gz
 
 
 tar xzvf libiconv-1.13.1.tar.gz
@@ -102,9 +102,8 @@ tar zxvf ghostscript-fonts-std-8.11.tar.gz
 sudo mkdir -p /usr/local/share/ghostscript/fonts
 sudo mv -f fonts/* /usr/local/share/ghostscript/fonts
 
-
-tar xzvf ImageMagick-6.6.7-0.tar.gz
-cd ImageMagick-6.6.7-0
+tar xzvf ImageMagick-6.8.1-2.tar.gz
+cd ImageMagick-6.8.1-2
 export CPPFLAGS=-I/usr/local/include
 export LDFLAGS=-L/usr/local/lib
 ./configure --prefix=/usr/local --disable-static --without-fontconfig --with-modules --without-perl --without-magick-plus-plus --with-quantum-depth=8 --with-gs-font-dir=/usr/local/share/ghostscript/fonts --disable-openmp
@@ -114,6 +113,6 @@ sudo make install
 cd ..
 
 cd ..
-rm -Rf magick-installer
+# rm -Rf magick-installer
 
 echo "ImageMagick successfully installed!"
